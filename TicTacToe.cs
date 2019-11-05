7258bb50cd52e3d6d5fe03cc65edcb3ec8f6651c
sabinecelina <45167220+sabinecelina@users.noreply.github.com>
2019-11-05 09:56:59 +0100

    changed

--- TicTacToe.cs
+++ TicTacToe.cs
@@ -63,13 +63,15 @@ namespace TicTacToe
 
         public static void getWinner()
         {
-            for (int i = 0; i < playfield.Length; i++)
-            {
-                if (playfield[0, 0] == playfield[0, 1] && playfield[0, 0] == playfield[0, 2] || playfield[1, 0] == playfield[1, 1] && playfield[1, 0] == playfield[1, 2] || playfield[2, 0] == playfield[2, 1] || playfield[2, 0] == playfield[2, 2] || playfield[0, 0] == playfield[1, 0] && playfield[0, 0] == playfield[2, 0] || playfield[0, 1] == playfield[1, 1] && playfield[0, 1] == playfield[2, 1] || playfield[0, 2] == playfield[1, 2] || playfield[0, 2] == playfield[2, 2]
-|| playfield[0, 0] == playfield[1, 1] && playfield[0, 0] == playfield[2, 2] || playfield[2, 0] == playfield[1, 1] && playfield[2, 0] == playfield[0, 2]
-)       Console.WriteLine("Der Sieger ist: " + player); 
-            }
 
+            if (playfield[0, 0] == "x" && playfield[0, 1] == "x" && playfield[0, 2] == "x" || playfield[1, 0] == "x" && playfield[1, 1] == "x" && playfield[1, 2] == "x" || playfield[2, 0] == "x" && playfield[2, 1] == "x")
+            {
+                Console.WriteLine(player + " is Winner");
+            }
+            else if (playfield[0, 0] == "o" && playfield[0, 1] == "o" && playfield[0, 2] == "o" || playfield[1, 0] == "o" && playfield[1, 1] == "o" && playfield[1, 2] == "o" || playfield[2, 0] == "o" && playfield[2, 1] == "o")
+            {
+                Console.WriteLine(player + " is Winner");
+            }
         }
         public static void Main()
         {
