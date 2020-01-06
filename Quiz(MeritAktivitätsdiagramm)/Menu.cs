b89fb1849e77dc54bf9161a8cf4a50dafe14176d
sabinecelina <45167220+sabinecelina@users.noreply.github.com>
2020-01-06 23:35:27 +0100

    json changed

--- Quiz(MeritAktivitätsdiagramm)/Menu.cs
+++ Quiz(MeritAktivitätsdiagramm)/Menu.cs
@@ -5,8 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
 using Newtonsoft.Json;
-
-
+using static System.Console;
+using System.IO;
 namespace Quiz
 {
     public class Menu
@@ -26,106 +26,123 @@ namespace Quiz
 
         public static void Main()
         {
+            menumenu.readQuestions();
             menu();
         }
-        public void QuizelementQuestion()
+        public void readQuestions()
         {
-
+            readQuizSingleChoiceJ();
         }
-        public static void menu()
+        private void readQuizSingleChoiceJ()
         {
-            Console.WriteLine("Please choose beetween: ");
-            Console.WriteLine("Press 1 to play the quiz");
-            Console.WriteLine("Press two to add a quizelement");
-            Console.WriteLine("Press three to exit the game");
-            string number = Console.ReadLine();
-            int x = 0;
-            x = Int32.Parse(number);
-            try
+            string path = "https://github.com/sabinecelina/Softwaredesign/blob/master/Quiz(MeritAktivit%C3%A4tsdiagramm)/Json/QuizelementTrueFalse.json";
+            List<QuizelementTrueFalse> quizList = new List<QuizelementTrueFalse>();
+
+            using (StreamReader r = new StreamReader(path))
             {
-                if (x == 1)
-                {
-                    menumenu.playQuiz();
-                    Console.WriteLine("If you want to continue the quiz press 1, if you want to go back to the menu press 2");
-                    Console.WriteLine("> ");
-                    var text = Console.ReadLine();
-                    int numberfromtext = Int32.Parse(text);
-                    if (numberfromtext == 1)
-                        menumenu.playQuiz();
-                    else if (numberfromtext == 2)
-                        menu();
-                }
-                else if (x == 2)
-                {
-                    menumenu.addQuestion();
-                    menu();
-                }
-                else if (x == 3)
-                {
-                    Debugger.Break();
-                }
+                string json = r.ReadToEnd();
+                quizList = JsonConvert.DeserializeObject<List<QuizelementTrueFalse>>(json);
             }
-            catch
+
+            foreach (QuizelementTrueFalse element in quizList)
             {
-                Console.WriteLine("invalid input,  please try it again");
+                quizelemente.Add(element);
+            }
+        }
+    public static void menu()
+    {
+        Console.WriteLine("Please choose beetween: ");
+        Console.WriteLine("Press one to play the quiz");
+        Console.WriteLine("Press two to add a quizelement");
+        Console.WriteLine("Press three to exit the game");
+        string number = Console.ReadLine();
+        int x = 0;
+        x = Int32.Parse(number);
+        try
+        {
+            if (x == 1)
+            {
+                menumenu.playQuiz();
+                Console.WriteLine("If you want to continue the quiz press 1, if you want to go back to the menu press 2");
+                Console.WriteLine("> ");
+                var text = Console.ReadLine();
+                int numberfromtext = Int32.Parse(text);
+                if (numberfromtext == 1)
+                    menumenu.playQuiz();
+                else if (numberfromtext == 2)
+                    menu();
+            }
+            else if (x == 2)
+            {
+                menumenu.addQuestion();
                 menu();
             }
+            else if (x == 3)
+            {
+                Debugger.Break();
+            }
         }
-        public void playQuiz()
+        catch
         {
-            menumenu.quizelemente[questionnumber].display();
-            string console = Console.ReadLine();
-            bool check = menumenu.quizelemente[questionnumber].checkAnswers(console);
-            if (check == true)
-            {
-                score += 1;
-                questionnumber += 1;
-                Console.WriteLine("Your Answer is correct!");
-                Console.WriteLine("___________________________ \n");
-            }
-            else
-            {
-                questionnumber += 1;
-                Console.WriteLine("Your Answer is not correct!");
-                Console.WriteLine("___________________________ \n");
-            }
-            Console.WriteLine("your score: " + score);
-            Console.WriteLine("previously answered questions:  " + questionnumber);
-            Console.WriteLine("___________________________ \n");
-        }
-        public void addQuestion()
-        {
-            Console.WriteLine("Please choose the quiztype");
-            Console.WriteLine("1: Text");
-            Console.WriteLine("2: True-False");
-            Console.WriteLine("3: Guess");
-            Console.WriteLine("4: Multiple answers");
-            Console.WriteLine("5: Single answers");
-            Console.WriteLine("6: return to menu");
-            string number = Console.ReadLine();
-            int x = 0;
-            x = Int32.Parse(number);
-            switch (x)
-            {
-                case 1:
-                    quizelemente.Add(addQuizelement.addQuizelementText());
-                    break;
-                case 2:
-                    quizelemente.Add(addQuizelement.addQuizelementTrueFalse());
-                    break;
-                case 3:
-                    quizelemente.Add(addQuizelement.addQuizelementGuess());
-                    break;
-                case 4:
-                    quizelemente.Add(addQuizelement.addQuizelementSingleChoice());
-                    break;
-                case 5:
-                    quizelemente.Add(addQuizelement.addQuizelementMultipleChoice());
-                    break;
-                case 6:
-                    menu();
-                    break;
-            }
+            Console.WriteLine("invalid input,  please try it again");
+            menu();
         }
     }
+    public void playQuiz()
+    {
+        menumenu.quizelemente[questionnumber].display();
+        string console = Console.ReadLine();
+        bool check = menumenu.quizelemente[questionnumber].checkAnswers(console);
+        if (check == true)
+        {
+            score += 1;
+            questionnumber += 1;
+            Console.WriteLine("Your Answer is correct!");
+            Console.WriteLine("___________________________ \n");
+        }
+        else
+        {
+            questionnumber += 1;
+            Console.WriteLine("Your Answer is not correct!");
+            Console.WriteLine("___________________________ \n");
+        }
+        Console.WriteLine("your score: " + score);
+        Console.WriteLine("previously answered questions:  " + questionnumber);
+        Console.WriteLine("___________________________ \n");
+    }
+    public void addQuestion()
+    {
+        Console.WriteLine("Please choose the quiztype");
+        Console.WriteLine("1: Text");
+        Console.WriteLine("2: True-False");
+        Console.WriteLine("3: Guess");
+        Console.WriteLine("4: Multiple answers");
+        Console.WriteLine("5: Single answers");
+        Console.WriteLine("6: return to menu");
+        string number = Console.ReadLine();
+        int x = 0;
+        x = Int32.Parse(number);
+        switch (x)
+        {
+            case 1:
+                quizelemente.Add(addQuizelement.addQuizelementText());
+                break;
+            case 2:
+                quizelemente.Add(addQuizelement.addQuizelementTrueFalse());
+                break;
+            case 3:
+                quizelemente.Add(addQuizelement.addQuizelementGuess());
+                break;
+            case 4:
+                quizelemente.Add(addQuizelement.addQuizelementSingleChoice());
+                break;
+            case 5:
+                quizelemente.Add(addQuizelement.addQuizelementMultipleChoice());
+                break;
+            case 6:
+                menu();
+                break;
+        }
+    }
+}
 }
